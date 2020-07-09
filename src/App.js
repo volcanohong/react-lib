91879c639ee693eef20cdfc0848fb28e0f0b3f28
volcanohong <volcanohong@gmail.com>
2020-07-09 08:56:18 -0700

    Button

--- src/App.js
+++ src/App.js
@@ -1,16 +1,51 @@
 import React from 'react';
 import OmButton from './component/OmButton';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  margin: {
+    margin: theme.spacing(1),
+  },
+  extendedIcon: {
+    marginRight: theme.spacing(1),
+  },
+}));
 
 function App() {
-
+  const classes = useStyles();
   return (
     <div className="">
-      <header className="om-bg-blue-200">
-        Component
-      </header>
-      <span  className="om-bg-red-200">123</span>
-      <div>
-        <OmButton name="Omega Button Test" onClick={function() { console.log('out click'); }}/>
+      <header className="om-p-5 om-text-lg">CORE5 Componet Lib</header>
+      <div className="om-container om-mx-auto om-px-4">
+        <div className="om-w-full om-py-5 om-px-2">
+          <span>Omega Buttons</span>
+        </div>
+        <div className="om-flex om-flex-row">
+          <OmButton className="om-px-2 om-py-4" name="Primary" onClick={function () { console.log('out click'); }} />
+          <OmButton className="om-px-2 om-py-4" name="Secondary" color="secondary" />
+          <OmButton className="om-px-2 om-py-4" name="Default" color="default" />
+          <OmButton className="om-px-2 om-py-4" name="Disabled" disabled />
+          {/* <input accept="image/*" className="om-hidden" type="file" id="btn-file-upload"/>
+          <lable htmlFor="btn-file-upload">
+            <OmButton className="om-px-2 om-py-4" name="Upload" color="primary" component="span" />
+          </lable> */}
+        </div>
+        <div className="om-flex om-flex-row">
+          <OmButton className="om-px-2 om-py-4" name="Outlined" variant="outlined" />
+          <OmButton className="om-px-2 om-py-4" name="Contained" variant="contained" />
+          <OmButton className="om-px-2 om-py-4" name="Text" variant="text" />
+        </div>
+        <div className="om-flex om-flex-row">
+          <OmButton className="om-px-2 om-py-4" name="Small" size="small" />
+          <OmButton className="om-px-2 om-py-4" name="Medium" size="medium" />
+          <OmButton className="om-px-2 om-py-4" name="Large" size="large" />
+        </div>
+        <div className="om-flex om-flex-row">
+          <OmButton className="om-px-2 om-py-4" icon="true" name="toc" color="default"/>
+          <OmButton className="om-px-2 om-py-4" icon="true" name="schedule" />
+          <OmButton className="om-px-2 om-py-4" icon="true" name="favorite" color="secondary"/>
+          <OmButton className="om-px-2 om-py-4" icon="true" name="delete" disabled/>
+        </div>
       </div>
     </div>
   );
--- src/component/OmButton.jsx
+++ src/component/OmButton.jsx
@@ -1,13 +1,35 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { Button, IconButton } from '@material-ui/core';
+
+const useStyles = makeStyles({
+  root: {
+    '&:focus': {
+      outline: 'none',
+    },
+  },
+});
 
 export default function OmButton(props) {
-  var variant = props.variant ? props.variant : "outlined";
-  var color = props.color ? props.color : "primary"
+  const classes = useStyles();
+  const variant = props.variant ? props.variant : "outlined";
+  const color = props.color ? props.color : "primary"
+
+  const isIcon = props.icon;
+  if (isIcon) {
+    return (
+      <div className={props.className}>
+        <IconButton classes={{ root: classes.root }} color={color} {...props}>
+          <i className="material-icons">{props.name}</i>
+        </IconButton>
+      </div>
+    );
+  }
+
   return (
-    <div className="">
-      <Button variant={variant} color={color} {...props}>{props.name}</Button>
+    <div className={props.className}>
+      <Button classes={{ root: classes.root }}
+        variant={variant} color={color} {...props}>{props.name}</Button>
     </div>
   );
-
 }
